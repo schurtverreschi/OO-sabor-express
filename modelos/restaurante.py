8f47996858fb5054417724a4c1fbc274c393d8c0
Schurt Verreschi <schurtverreschi@gmail.com>
2026-09-08 21:16:13 -0300

    feat(modelas): cria metodo proprio para lista restaurantes
    Metodo exibe lista dos resuteantes incluido de forma organizada

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,17 +1,26 @@
 class Restaurante:
+
+    restaurantes = []  # noqa: RUF012
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.status = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return (
             f"{self.nome} | {self.categoria} | {'Ativo' if self.status else 'Inativo'}"
         )
 
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(
+                f"{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.status}"
+            )
+
 
 restaurante_praca = Restaurante("Praça", "Italiana")
 restaurante_pizza = Restaurante("Rhino", "Pizzaria")
 
-print(restaurante_pizza)
-print(restaurante_praca)
+Restaurante.listar_restaurantes()
